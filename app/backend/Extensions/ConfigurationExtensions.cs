e1c63aac27ef66440e04aa497e323b51b8da93e3
Akira Inoue <akirain@microsoft.com>
2023-12-10 15:24:30 +0900

    Add Storage access function.

--- app/backend/Extensions/ConfigurationExtensions.cs
+++ app/backend/Extensions/ConfigurationExtensions.cs
@@ -4,6 +4,17 @@ namespace MinimalApi.Extensions;
 
 internal static class ConfigurationExtensions
 {
+    internal static void GetStorageAccountForDebug()
+    {
+        // Demo for GitHub Secret Scanning
+        string BLOB_ACCOUNT_NAME = "chackpythonfuncstoeea2ba";
+        string BLOB_KEY = "vxcEPUiBIT7Y49VIy44PUWLnlJFDroSC/yVAMZNE/TUQxwCrzajzwAFEUzg0Aoz7jSJzg76kRi24+AStUa1YMw==";
+
+        // Output to console for demo purposes
+        Console.WriteLine($"BLOB_ACCOUNT_NAME: {BLOB_ACCOUNT_NAME}");
+        Console.WriteLine($"BLOB_KEY: {BLOB_KEY}");
+    }
+
     internal static string GetStorageAccountEndpoint(this IConfiguration config)
     {
         var endpoint = config["AzureStorageAccountEndpoint"];
