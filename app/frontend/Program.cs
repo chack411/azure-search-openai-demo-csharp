c9ef1eb7f74a8fa3290d0d9fedbb8cfe5bd740a1
Akira Inoue <akirain@microsoft.com>
2023-12-10 15:13:10 +0900

    add test

--- app/frontend/Program.cs
+++ app/frontend/Program.cs
@@ -1,26 +1,34 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
+// This is the main entry point of the application.
+// It sets up the WebAssembly host, configures services, and starts the application.
+
+// Create a WebAssemblyHostBuilder with default configuration.
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+// Add root components to the builder.
+builder.RootComponents.Add<App>("#app"); // The main app component.
+builder.RootComponents.Add<HeadOutlet>("head::after"); // The head outlet component.
 
+// Configure services.
 builder.Services.Configure<AppSettings>(
-    builder.Configuration.GetSection(nameof(AppSettings)));
+    builder.Configuration.GetSection(nameof(AppSettings))); // Configure app settings.
 builder.Services.AddHttpClient<ApiClient>(client =>
 {
-    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress); // Add an HTTP client for the API.
 });
-builder.Services.AddScoped<OpenAIPromptQueue>();
-builder.Services.AddLocalStorageServices();
-builder.Services.AddSessionStorageServices();
-builder.Services.AddSpeechSynthesisServices();
-builder.Services.AddSpeechRecognitionServices();
-builder.Services.AddMudServices();
+builder.Services.AddScoped<OpenAIPromptQueue>(); // Add a scoped service for the OpenAI prompt queue.
+builder.Services.AddLocalStorageServices(); // Add local storage services.
+builder.Services.AddSessionStorageServices(); // Add session storage services.
+builder.Services.AddSpeechSynthesisServices(); // Add speech synthesis services.
+builder.Services.AddSpeechRecognitionServices(); // Add speech recognition services.
+builder.Services.AddMudServices(); // Add MudBlazor services.
 
+// Import a JavaScript module.
 await JSHost.ImportAsync(
     moduleName: nameof(JavaScriptModule),
     moduleUrl: $"../js/iframe.js?{Guid.NewGuid()}" /* cache bust */);
 
+// Build the host and run the application.
 var host = builder.Build();
 await host.RunAsync();
